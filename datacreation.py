5b87afdb8823be90f1bda8dd6cdde09af2922e51
Max-Hi <levin.hirsch@web.de>
2025-02-06 13:01:48 +0100

    improved datacreation

--- datacreation.py
+++ datacreation.py
@@ -2,19 +2,68 @@ import numpy as np
 
 from typing import Union
 
-def circle(phi: Union[float, np.ndarray]) -> Union[float, np.ndarray]:
+def circle(phi: Union[float, np.ndarray], r: float = 1) -> Union[float, np.ndarray]:
+    """
+    Parametric equation of a circle at hight z=1.
+    
+    Args:
+        phi: Angle in the circle
+        r: Circle radius
+    
+    Returns:
+        Tuple of (x, y, z) coordinates
+    """
     if isinstance(phi, float):
-        return (np.cos(phi), np.sin(phi), 1)
+        return (r*np.cos(phi), r*np.sin(phi), 1)
     elif isinstance(phi, np.ndarray):
-        return (np.cos(phi), np.sin(phi), np.ones(len(phi)))
+        return (r*np.cos(phi), r*np.sin(phi), np.ones(len(phi)))
     else:
         raise ValueError
 
-def sphere(phi: Union[float, np.ndarray], theta: Union[float, np.ndarray]) -> Union[float, np.ndarray]:
-    return (np.cos(phi)*np.sin(theta), np.sin(phi)*np.sin(theta), np.cos(theta))
+def sphere(phi: Union[float, np.ndarray], theta: Union[float, np.ndarray], r: float = 1) -> Union[float, np.ndarray]:
+    """
+    Parametric equation of a sphere.
+    
+    Args:
+        phi, theta: the two angles characterizing the sphere in polar coordinates
+        r: Sphere radius
+    
+    Returns:
+        Tuple of (x, y, z) coordinates
+    """
+    return (r*np.cos(phi)*np.sin(theta), r*np.sin(phi)*np.sin(theta), r*np.cos(theta))
 
-def bowl(phi: Union[float, np.ndarray], theta: Union[float, np.ndarray]) -> Union[float, np.ndarray]:
-    return (np.cos(phi)*theta, np.sin(phi)*theta, theta**2)
+def bowl(phi: Union[float, np.ndarray], z: Union[float, np.ndarray], r: float = 1) -> Union[float, np.ndarray]:
+    """
+    Parametric equation of a bowl like structure that is parabolic in z coordinate and circular in the xy plane.
+    
+    Args:
+        phi: Angle around the tube's cross-section (minor circle)
+        z: z coordinate
+        r: Circle radius at z=1
+    
+    Returns:
+        Tuple of (x, y, z) coordinates
+    """
+    return (r*np.cos(phi)*z, r*np.sin(phi)*z, z**2)
 
+def torus(phi: Union[float, np.ndarray], theta: Union[float, np.ndarray], R: float = 2, r: float = 1) -> Union[float, np.ndarray]:
+    """
+    Parametric equation of a torus.
+    
+    Args:
+        phi: Angle around the tube's cross-section (minor circle)
+        theta: Angle around the major circle
+        R: Major radius (distance from the center of the tube to the center of the torus)
+        r: Minor radius (radius of the tube)
+    
+    Returns:
+        Tuple of (x, y, z) coordinates
+    """
+    x = (R + r * np.cos(phi)) * np.cos(theta)
+    y = (R + r * np.cos(phi)) * np.sin(theta)
+    z = r * np.sin(phi)
+    
+    return (x, y, z)
 
 
